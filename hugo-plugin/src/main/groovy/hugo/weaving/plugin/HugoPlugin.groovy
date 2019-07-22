68fb044a525300713af4d05962c363d3b06fa146
Tin Tran <thuutin@gmail.com>
2019-07-22 20:37:35 +0700

    Revert

--- hugo-plugin/src/main/groovy/hugo/weaving/plugin/HugoPlugin.groovy
+++ hugo-plugin/src/main/groovy/hugo/weaving/plugin/HugoPlugin.groovy
@@ -1,40 +1,28 @@
 package hugo.weaving.plugin
 
-import com.android.build.gradle.AppExtension
 import com.android.build.gradle.AppPlugin
-import com.android.build.gradle.LibraryExtension
 import com.android.build.gradle.LibraryPlugin
-import com.android.build.gradle.api.BaseVariant
+import org.aspectj.bridge.IMessage
+import org.aspectj.bridge.MessageHandler
+import org.aspectj.tools.ajc.Main
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.compile.JavaCompile
 
 class HugoPlugin implements Plugin<Project> {
-
-  @Override
-  void apply(Project project) {
+  @Override void apply(Project project) {
     def hasApp = project.plugins.withType(AppPlugin)
     def hasLib = project.plugins.withType(LibraryPlugin)
     if (!hasApp && !hasLib) {
       throw new IllegalStateException("'android' or 'android-library' plugin required.")
     }
 
-    def transform = new HugoTransform(project, isEnabled(project))
-
-    if (hasLib) {
-      def android = project.extensions.getByType(LibraryExtension)
-      android.registerTransform(transform)
-
-      android.libraryVariants.all { BaseVariant variant ->
-        configureCompileJavaTask(variant, variant.javaCompile, transform)
-      }
+    final def log = project.logger
+    final def variants
+    if (hasApp) {
+      variants = project.android.applicationVariants
     } else {
-      def android = project.extensions.getByType(AppExtension)
-      android.registerTransform(transform)
-
-      android.applicationVariants.all { BaseVariant variant ->
-        configureCompileJavaTask(variant, variant.javaCompile, transform)
-      }
+      variants = project.android.libraryVariants
     }
     project.repositories {
       maven { url "https://jitpack.io" }
@@ -45,17 +33,52 @@ class HugoPlugin implements Plugin<Project> {
       debugImplementation 'org.aspectj:aspectjrt:1.8.6'
       implementation 'com.github.skedgo.hugo:hugo-annotations:1.2.3'
     }
+
     project.extensions.create('hugo', HugoExtension)
-  }
 
-  private static boolean isEnabled(Project project) {
-    if(project.hasProperty("hugo") && project.hugo.hasProperty("enabled")) {
-      return project.hugo.enabled;
+    variants.all { variant ->
+      if (!variant.buildType.isDebuggable()) {
+        log.debug("Skipping non-debuggable build type '${variant.buildType.name}'.")
+        return;
+      } else if (!project.hugo.enabled) {
+        log.debug("Hugo is not disabled.")
+        return;
+      }
+
+      JavaCompile javaCompile = variant.javaCompile
+      javaCompile.doLast {
+        String[] args = [
+            "-showWeaveInfo",
+            "-1.5",
+            "-inpath", javaCompile.destinationDir.toString(),
+            "-aspectpath", javaCompile.classpath.asPath,
+            "-d", javaCompile.destinationDir.toString(),
+            "-classpath", javaCompile.classpath.asPath,
+            "-bootclasspath", project.android.bootClasspath.join(File.pathSeparator)
+        ]
+        log.debug "ajc args: " + Arrays.toString(args)
+
+        MessageHandler handler = new MessageHandler(true);
+        new Main().run(args, handler);
+        for (IMessage message : handler.getMessages(null, true)) {
+          switch (message.getKind()) {
+            case IMessage.ABORT:
+            case IMessage.ERROR:
+            case IMessage.FAIL:
+              log.error message.message, message.thrown
+              break;
+            case IMessage.WARNING:
+              log.warn message.message, message.thrown
+              break;
+            case IMessage.INFO:
+              log.info message.message, message.thrown
+              break;
+            case IMessage.DEBUG:
+              log.debug message.message, message.thrown
+              break;
+          }
+        }
+      }
     }
-    return true;
-  }
-
-  private static configureCompileJavaTask(BaseVariant variant, JavaCompile javaCompileTask, HugoTransform transform) {
-    transform.putJavaCompileTask(variant.flavorName, variant.buildType.name, javaCompileTask)
   }
 }
